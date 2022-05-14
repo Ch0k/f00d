344dc238044a6ee03c225a3877b065bcdfcd7a59
andreevsergey <andreev,sv89@yandex.ru>
2022-05-14 23:37:50 +0300

    add line items controller and spec

---
+++ app/controllers/line_items_controller.rb
@@ -0,0 +1,30 @@
+class LineItemsController < ApplicationController
+  before_action :set_line_item, only: [:show, :edit, :update, :destroy]
+  include CurrentCart
+  before_action :set_cart, only: [:create]
+
+  def create
+    product = Product.find(params[:product_id])
+    @line_item = @cart.add_product(product)
+    respond_to do |format|
+      if @line_item.save
+        format.html { redirect_to store_index_url }
+        format.json { render :show, status: :created, location: @line_item }
+        #format.js { @current_item = @line_item }
+      else
+        format.html { render :new }
+        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_line_item
+    @line_item = LineItem.find(params[:id])
+  end
+
+  def line_item_params
+    params.require(:line_item).permit(:product_id)
+  end
+end
