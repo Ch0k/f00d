6df094d7c14f0374af364cc0dc460ff7ed51a59f
andreevsergey <andreev,sv89@yandex.ru>
2022-05-20 10:57:54 +0300

    add model order and add model tests

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -2,5 +2,6 @@ require 'rails_helper'
 
 RSpec.describe LineItem, type: :model do
   it { should belong_to(:product) }
-  it { should belong_to(:cart) }
+  it { should belong_to(:cart).optional }
+  it { should belong_to(:order).optional }
 end
