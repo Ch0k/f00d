6b9f6e64606d800c67b7afb6f759d29d9dce5a1b
andreevsergey <andreev,sv89@yandex.ru>
2022-05-22 00:58:32 +0300

    add first_name, last_name, phone to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   include CurrentCart
   before_action :set_cart
-  #before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 #
   #def after_sign_in_path_for(resource)
   #  user_path(resource)
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   #
   #protected
 #
-  #def configure_permitted_parameters
-  #  devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-  #end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :last_name, :first_name, :phone])
+  end
 end
