b3f8a746c400c047a43f456fa9f7d6a2fe29d1e6
andreevsergey <andreev,sv89@yandex.ru>
2022-06-03 14:18:17 +0300

    add users objects, add_order and cart

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,24 @@
 products = Product.create([{ title: 'Pizza1', body: 'Pizza with cheeze', price: '15' },
                            { title: 'Pizza2', body: 'Pizza with cheeze', price: '20' },
                            { title: 'Pizza3', body: 'Pizza with cheeze', price: '30' }])
+
+users = User.create!(
+  [
+    { email: 'admin2@exemple.com', password: '123456', first_name: 'Вася', last_name: 'Печкин', phone: '123' },
+    {  email: 'customer@exemple.com', password: '123456', first_name: 'Петя', last_name: 'Кусин', phone: '123' },
+    {  email: 'manager@exemple.com', password: '123456', first_name: 'Гона', last_name: 'Тусин', phone: '123' }
+  ]
+)
+
+cart = Cart.create
+
+orders = Order.create!([
+                         { user_id: users[2].id, address: '123 123 123' }
+                       ])
+
+line_items = LineItem.create!([
+                                { product_id: products[1].id, cart_id: cart.id },
+                                { product_id: products[2].id, cart_id: cart.id },
+                                { product_id: products[1].id, order_id: orders[1].id },
+                                { product_id: products[2].id, order_id: orders[1].id }
+                              ])
